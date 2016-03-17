cb4494c5d63c19a9055cdaab9e18f9cb5a505daf
a.b.salikhov@gmail.com <a.b.salikhov@gmail.com>
2016-03-17 18:20:48 +0500

    add area

---
+++ UzexSurvey/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace UzexSurvey.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                name: "admin",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "UzexSurvey.Areas.Admin.Controllers" }
+            );
+            
+        }
+    }
+}
