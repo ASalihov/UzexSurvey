8af06f8af4b360f02abe6425457ae4855397518e
a.b.salikhov@gmail.com <a.b.salikhov@gmail.com>
2016-03-23 00:21:41 +0500

    Added IoC

---
+++ UzexSurvey/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using Ninject.Web.Common;
+using DAL;
+using DAL.Contracts;
+using DAL.Repositories;
+
+namespace UzexSurvey.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            _kernel.Bind<IUoW>().To<UoW>();
+            //_kernel.Bind<IQuizRepository>().To<QuizRepository>();
+        }
+    }
+}
