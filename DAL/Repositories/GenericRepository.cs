db0c05504fe11f862315116f23dd9b6ed251e574
a.b.salikhov@gmail.com <a.b.salikhov@gmail.com>
2016-03-23 21:28:55 +0500

    add untracked files

---
+++ DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Contracts;
+
+namespace DAL.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>();
+        }
+
+        public virtual IEnumerable<T> Find(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
