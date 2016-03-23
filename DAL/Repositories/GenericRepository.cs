f3c224fd36ee87a90d95eada91d4a8bd24979aa5
a.b.salikhov@gmail.com <a.b.salikhov@gmail.com>
2016-03-23 22:43:23 +0500

    including genericrepo in project

---
+++ DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Data.Entity;
+using DAL.Contracts;
+
+namespace DAL.Repositories
+{
+    public abstract class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+
+        public GenericRepository (DbContext context)
+	    {
+            Context = context;
+	    }
+
+        public virtual T GetById(int id)
+        {
+            return Context.Set<T>().Find(id);
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>().ToList();
+        }
+
+        public virtual IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().Where(predicate);
+        }
+
+        public virtual void Add(T entity)
+        {
+            Context.Set<T>().Add(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+        }
+    }
+}
