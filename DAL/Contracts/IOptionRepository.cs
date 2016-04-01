cf6c2769fa445ff7a5819ca89d5ec92d17eb8958
a.b.salikhov@gmail.com <a.b.salikhov@gmail.com>
2016-04-01 15:57:42 +0500

    error

---
+++ DAL/Contracts/IOptionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Contracts
+{
+    public interface IOptionRepository : IRepository<Option>
+    {
+        IEnumerable<Option> GetByQuestionId(int questionId);
+    }
+}
